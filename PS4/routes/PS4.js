d7f3dc95f03c16f6d6c750e72eb6ae7d614a075d
Beatrice Tanaga <btanagaa@hotmail.com>
2019-06-08 16:10:51 -0400

    PS4 final submission

---
+++ PS4/routes/PS4.js
@@ -0,0 +1,20 @@
+var rp = require('request-promise');
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/ps4', function(req, res, next) {
+    rp('https://api.flickr.com/services/rest/?api_key=918b154854622e2f09a23016c8740830&method=flickr.photos.search&format=json&nojsoncallback=1&tags=dog')
+        .then(function (responseText) {
+            //console.log( responseText );
+            let responseObject = JSON.parse( responseText );
+
+            // TODO: Render the pug template and pass the object
+            res.render('partC', { data: responseObject});
+        })
+        .catch(function (err) {
+            console.log( "Error: ", err );
+        });
+});
+
+module.exports = router;
