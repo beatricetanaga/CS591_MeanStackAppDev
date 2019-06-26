8a07436771335328870bd6782f4369b25aa9a69c
Beatrice Tanaga <btanagaa@hotmail.com>
2019-06-25 21:42:05 -0400

    PS8 submission

--- PS8/src/app/app.component.ts
+++ PS8/src/app/app.component.ts
@@ -9,15 +9,20 @@ import { ChangeDetectorRef } from '@angular/core';
 })
 export class AppComponent {
   flickrTag: string;
+  lastSearch: string;
   photos: any;
   constructor(private httpClient: HttpClient, private ref: ChangeDetectorRef) {
   }
   // title = 'PS6';
 
   get_images(): any {
-    if ( this.flickrTag.length < 1 ) return;
-    return this.httpClient.get('/api/ps4/' + this.flickrTag).subscribe((res) => {
+    if ( this.flickrTag.length < 1 || this.lastSearch === this.flickrTag) {
+      console.log('same tag');
+      return;
+    }
+    return this.httpClient.get('/api/ps4/' + this.flickrTag).subscribe((res:any) => {
       console.log(res);
+      this.lastSearch = this.flickrTag;
       this.photos = res.photos.photo;
       this.ref.detectChanges();
     });
